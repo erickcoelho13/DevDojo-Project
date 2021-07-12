e7e0d1952d0833c50af946157692bd0da5541543
Erick Coelho <erck.coelho@gmail.com>
2021-07-12 13:47:54 -0300

    Third Commit - Delete Method OK

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -8,6 +8,7 @@ import lombok.extern.log4j.Log4j2;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,4 +43,10 @@ public class AnimeController {
     	public ResponseEntity<Anime> save(@RequestBody Anime anime) {
     			return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED) ;
     }
+    
+    @DeleteMapping(path = "/{id}")
+     	public ResponseEntity<Void> delete(@PathVariable long id) {
+     		animeService.delete(id);	
+    		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+     }
 }
--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -33,4 +33,8 @@ public class AnimeService {
 			return anime;
 		}
 
+		public void delete(long id) {
+				animes.remove(findById(id));
+		}
+
 }
