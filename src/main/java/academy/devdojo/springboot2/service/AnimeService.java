a8518b28a21993fba33c29a7fb3a2569b3378b23
Erick Coelho <erck.coelho@gmail.com>
2021-07-12 10:26:20 -0300

    Second Commit

---
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -0,0 +1,36 @@
+package academy.devdojo.springboot2.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import academy.devdojo.springboot2.domain.Anime;
+
+@Service
+public class AnimeService {
+		private static List<Anime> animes;
+		static {
+			 animes = new ArrayList<>(List.of(new Anime(1L, "Boku No Hero"), new Anime(2L, "Berserk")));
+		}
+		//private final AnimeRepository animeRepository;
+		public List<Anime> listAll() {
+			return  animes;
+		}
+
+		public Anime findById(long id) {
+			return  animes.stream().filter(anime -> anime.getId().equals(id))
+					.findFirst()
+					.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime ID Not Found"));
+		}
+
+		public Anime save(Anime anime) {
+			anime.setId(ThreadLocalRandom.current().nextLong(3, 1000000));
+			animes.add(anime);
+			return anime;
+		}
+
+}
