67f6f90b3fd7973141fc5ec2140ecc974216df82
Erick Coelho <erck.coelho@gmail.com>
2021-07-14 11:02:58 -0300

    Class 15

--- src/main/java/academy/devdojo/springboot2/mapper/AnimeMapper.java
+++ src/main/java/academy/devdojo/springboot2/mapper/AnimeMapper.java
@@ -1,5 +1,8 @@
 package academy.devdojo.springboot2.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
 import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.requests.AnimePostRequestBody;
 import academy.devdojo.springboot2.requests.AnimePutRequestBody;
